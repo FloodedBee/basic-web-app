c0da025610098fe22f29de32e567c11f0771988d
FloodedBee <aamirand@andrew.cmu.edu>
2024-03-14 16:24:16 -0400

    added new code for handling largest num

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -21,5 +21,15 @@ export default function QueryProcessor(query: string): string {
     }
     
   }
+  if (query.toLowerCase().includes("which of the following numbers is the largest:")) {
+    const arr: string[] = query.split(" ")
+    const intVal1: number = parseInt(arr[8]);
+    const intVal2: number = parseInt(arr[9]);
+    const intVal3: number = parseInt(arr[10]);
+    return (Math.max(intVal1, intVal2, intVal3)).toString()
+  }
+
   return "";
+
+  
 }
